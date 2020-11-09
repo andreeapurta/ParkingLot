50cb95b161818666741df0c65a2e8a85d0ad27d8
andreeapurta <andreea.purta@gmail.com>
2020-11-09 12:33:19 +0200

    UserBean, CarBean, CarDetails, UserDetails

---
+++ src/main/java/com/park/parkinglot/common/CarDetails.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.parkinglot.common;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Andreea Purta
+ */
+public class CarDetails implements Serializable{
+
+    private Integer id;
+    private String licensePlate;
+    private String parkingSpot;
+    private String username;
+
+    public CarDetails(Integer id, String licensePlate, String parkingSpot, String username) {
+        this.id = id;
+        this.licensePlate = licensePlate;
+        this.parkingSpot = parkingSpot;
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
